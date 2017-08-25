425e4e9324ca73d407f546cb8e692d5548a88862
Shubhankeet Senapati <30683426+Shubhankeet@users.noreply.github.com>
2017-08-25 13:44:36 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,12 +5,80 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+
+var articleOne = {
+    tilte: 'Article one 1:Shubhankeet Senapati',
+    heading: 'Article One',
+    date: 'August 24,2016',
+    content: `
+    <p>
+                This is the content for my first article.Hello Everyone.Myself Shhubhankeet Senapati.I am from Odisha.I was born and brought up in Rourkela,Sundergarh,Odisha.Rourkela is one of the most beautiful place in India;one of the cleanest city filed with greenery everywhere.I completed my high school at Ispat English Medium School,a well recognized school in Odisha.
+            </p>
+            <p>
+              Every day was like a new day in  Rourkela;filled with enjoyment and fun. 
+           </p>
+           <p>
+               Whether it was at school or outside school,playing different sports,hanging out with friends,etc was just awesome!!
+           </p>
+           <p>
+               I like to play football and volleyball out of which i have played volleyball the most.I still play that game whenenver i go home during my vaccations,with my friends.
+           </p>`
+};
+function createTemplate(data){
+var title=data.title;
+var date=data.date;
+var heading=data.heading;
+var content=data.content;
+
+var htmlTemplate = `
+<html>
+    <head>
+        <title>
+            S{title}
+        </title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link href="/ui/style.css" rel="stylesheet" />
+<style>
+    .container{
+        max-width: 800px;
+        margin: 0 auto;
+        color: red;
+        font-family: sans-serif;
+        padding-top: 60px;
+        padding-left: 20px;
+        padding-right: 20px;
+}
+     </style>
+    </head>
+    <body>
+        <div class="container">
+        <div>
+            <a href="/">Home</a>
+        </div>
+        <hr/>
+        <h3>
+            S{heading}
+        </h3>
+        <div>
+            S{date}
+        </div>
+        <div>
+           S{content}
+        </div>
+        </div>
+    </body>
+</html>
+`;
+return htmlTemplate;
+}
+
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 app.get('/article-one', function (req, res){
-    res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+    res.send(crreateTemplate(articleOne));
 });
 
 app.get('/article-two', function (req, res){
